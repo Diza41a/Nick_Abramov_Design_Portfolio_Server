3280707890fcac7e84e5ec29cba9a4efcc5260f9
Davyd Zakorchennyi <davyd.zakorchennyi@chartboost.com>
2023-12-22 16:34:57 -0500

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './main/modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
