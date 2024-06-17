5819135b4c3fe67aeb052cfa3a5d18130e34135f
Davyd Zakorchennyi <Diza41a@mail.com>
2024-06-17 13:27:17 -0400

    @feat: FAQ - Reorder FAQs endpoint; Moved reorder logic to a global util (as the logic is reused);

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,18 @@
+import { Model } from 'mongoose';
+
+type OrderedDocument = {
+  order: number;
+  [key: string]: any;
+};
 export const reorderDocument = async (
   documentId: string,
   newOrder: number,
-  model: any,
+  model: Model<OrderedDocument>,
 ): Promise<any> => {
   const document = await model.findById(documentId);
   const oldOrder = document.order;
 
-  if (oldOrder === newOrder) {
-    return;
-  }
-
-  const incAmount = newOrder < oldOrder ? 1 : -1;
+  if (oldOrder === newOrder) return;
 
   const newOrderIsLessFilter = {
     order: { $gte: newOrder, $lt: oldOrder },
@@ -18,16 +20,17 @@ export const reorderDocument = async (
   const newOrderIsMoreFilter = {
     order: { $gt: oldOrder, $lte: newOrder },
   };
+
+  const incrementAmount = newOrder < oldOrder ? 1 : -1;
+  const update = { $inc: { order: incrementAmount } };
   const filter = {
     $and: [
       { order: { $ne: oldOrder } },
       newOrder < oldOrder ? newOrderIsLessFilter : newOrderIsMoreFilter,
     ],
   };
-
-  const update = { $inc: { order: incAmount } };
-
   await model.updateMany(filter, update);
+
   const reorderedDocument = await model.findByIdAndUpdate(
     documentId,
     { order: newOrder },
