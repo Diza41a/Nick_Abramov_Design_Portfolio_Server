e73e5ca1d26a48c19084492fb4c02cbba0978960
Davyd Zakorchennyi <Diza41a@mail.com>
2024-12-03 12:39:27 -0600

    @chore: Setup the development MongoDB Atlas cluster and connect to it if in development Node environment;

--- src/main/database.provider.ts
+++ src/main/database.provider.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-const { MONGODB_URI, DB_NAME } = process.env;
+const { NODE_ENV, MONGODB_URI_PROD, MONGODB_URI_DEV, DB_NAME } = process.env;
+
+const CONNECTION_STRING =
+  NODE_ENV === 'production' ? MONGODB_URI_PROD : MONGODB_URI_DEV;
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(MONGODB_URI, {
+      mongoose.connect(CONNECTION_STRING, {
         dbName: DB_NAME,
       }),
   },
